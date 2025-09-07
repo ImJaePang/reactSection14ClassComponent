13022c5ac29502ded95721d74098b353bb623921
ImJaePang <narushel@naver.com>
2025-09-07 21:27:17 +0900

    componentAct

--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,12 @@ import classes from './User.module.css';
 import {Component} from 'react';
 
 class User extends Component {
+
+  componentWillUnmount(){
+    // unmount happen
+    // console.log("unmount user : " + this.props.name);
+  }
+
   render(){
     return <li className={classes.user}>{this.props.name}</li>;  
   }
