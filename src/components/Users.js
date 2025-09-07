294ea396b500d3a42cc89a25a335898749deccac
ImJaePang <narushel@naver.com>
2025-09-07 09:17:14 +0900

    usersChange

--- src/components/Users.js
+++ src/components/Users.js
@@ -9,26 +9,38 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-class Users extends Component (){
+class Users extends Component {
 
   constructor(){
+    super();
     this.state = {
-      showUsers:true
+      showUsers:true,
+      more : 'Test'
     };
   }
 
   toggleUsersHandler(){
-    this.useState()
+    this.setState((curState)=>{
+      return {showUsers : !curState.showUsers};
+    });
   }
 
   render(){
+
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
     
     return (
       <div className={classes.users}>
-        <button onClick={toggleUsersHandler}>
-          {showUsers ? 'Hide' : 'Show'} Users
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
         </button>
-        {showUsers && usersList}
+        {this.state.showUsers && usersList}
       </div>
     );
 
